46831443c1870ee239cd443d15e066fd2a9a66f1
Mark G <markghaida@gmail.com>
2020-11-08 00:46:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,35 @@
 def using_include(array, element)
 
-end
+  array.include?(element)
+end	end
 
-def using_sort(array)
 
-end
+def using_sort(array)	def using_sort(array)
+
+  sorted_array = array.sort
+  sorted_array
+end	end
+
 
 def using_reverse(array)
 
+  array.reverse
 end
+
 
 def using_first(array)
 
+  array.first
 end
+
 
 def using_last(array)
 
+  array.last
 end
+
 
 def using_size(array)
 
+  array.size
 end
